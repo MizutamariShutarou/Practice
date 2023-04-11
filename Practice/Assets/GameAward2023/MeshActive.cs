214c10285a276ac68667ea974aa50f3a798555c7
MizutamariShutarou <shutaro0415@outlook.jp>
2023-04-11 22:10:16 +0900

    [update] MeshFilterのshareMeshを直接保存してみるがJsonに対応していなかった

--- Practice/Assets/GameAward2023/MeshActive.cs
+++ Practice/Assets/GameAward2023/MeshActive.cs
@@ -2,32 +2,45 @@ using System;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
 using System.Threading;
+using System.IO;
 
-/*public class MeshActive : MonoBehaviour
+public class MeshActive : MonoBehaviour
 {
     [SerializeField]
     private GameObject _panel;
     private MeshFilter _meshFilter;
     private Mesh _myMesh;
 
+    DataManager _dataManager;
+
 
     private void Start()
     {
         _panel.SetActive(false);
-        var ct = this.GetCancellationTokenOnDestroy();
-
         _meshFilter = gameObject.GetComponent<MeshFilter>();
-        _myMesh = NewMeshManager._myMesh;
-        AcyncStart(ct).Forget();
+        _myMesh = new Mesh();
+
+        _myMesh.SetVertices(NewMeshManager.MyVertices);
+
+        _meshFilter.sharedMesh = _myMesh;
+
+        foreach(var v in NewMeshManager.MyVertices)
+        {
+            Debug.Log(v);
+        }
+
+        // var ct = this.GetCancellationTokenOnDestroy();
+
+        // AcyncStart(ct).Forget();
     }
     private async UniTask AcyncStart(CancellationToken ct)
     {
         try
         {
-            _meshFilter.mesh = _myMesh;
+            _dataManager.Load();
         }
 
-        catch(ArgumentException e)
+        catch (NullReferenceException e)
         {
             Debug.LogError(e.Message);
             _panel.SetActive(true);
@@ -35,9 +48,65 @@ using System.Threading;
 
         finally
         {
-            Debug.Log($"{NewMeshManager.MeshName}çÏê¨");
-            
+            Debug.Log(_dataManager);
+            //_myMesh = _saveData._mesh;
+            //_meshFilter = _saveData._meshFilter;
+            //Debug.Log(_saveData._prefabName);
+            //Debug.Log(_saveData._mesh);
+            //Debug.Log(_saveData._meshFilter);
+
             await UniTask.CompletedTask;
         }
+    }
+}
+/*[Serializable]
+public class SaveMeshFilter
+{
+    private Mesh sharedMesh = null;
+    private Mesh mesh = null;
+
+    public static void Save(MeshFilter saveObj, string fileName)
+    {
+        var s = saveObj.sharedMesh;
+        var m = saveObj.mesh;
+
+        string jsonS = JsonUtility.ToJson(s);
+        string jsonM = JsonUtility.ToJson(m);
+
+        StreamWriter streamWriterS = new StreamWriter(fileName + "\\" + "s" + ".json");
+        StreamWriter streamWriterM = new StreamWriter(fileName + "\\" + "m" + ".json");
+
+        streamWriterS.Write(jsonS);
+        streamWriterM.Write(jsonM);
+
+        streamWriterS.Flush();
+        streamWriterM.Flush();
+
+        streamWriterS.Close();
+        streamWriterM.Close();
+    }
+    public static MeshFilter Load(string fileName)
+    {
+        if (File.Exists(fileName))
+        {
+            var streamReaderS = new StreamReader(fileName + "\\" + "s" + ".json");
+            var streamReaderM = new StreamReader(fileName + "\\" + "m" + ".json");
+
+            string dataS = streamReaderS.ReadToEnd();
+            string dataM = streamReaderM.ReadToEnd();
+
+            streamReaderS.Close();
+            streamReaderM.Close();
+
+            var s = JsonUtility.FromJson<Mesh>(dataS);
+            var m = JsonUtility.FromJson<Mesh>(dataM);
+
+            var result = new MeshFilter();
+            result.sharedMesh = s;
+            result.mesh = m;
+
+            return result;
+        }
+        return null;
     }
 }*/
