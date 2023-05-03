f9348bb69deb0e74bc475aacfd3850b2dfc65d2e
MizutamariShutarou <shutaro0415@outlook.jp>
2023-05-03 19:42:05 +0900

    非同期でのAddressable Assets

---
+++ Practice/Assets/AddressableAssetsSample/AsyncOperationHandleAwaitSample.cs
@@ -0,0 +1,32 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.UI;
+
+public class AsyncOperationHandleAwaitSample : MonoBehaviour
+{
+    /// <summary>
+    /// ì«Ç›çûÇﬁëŒè€ÇÃAssetReference
+    /// </summary>
+    [SerializeField] AssetReference _target;
+
+    [SerializeField] private RawImage _image;
+
+    private void Start()
+    {
+        var token = this.GetCancellationTokenOnDestroy();
+        InitializeAsync(_target, token).Forget();
+    }
+
+    private async UniTaskVoid InitializeAsync(
+        AssetReference target,
+        CancellationToken token)
+    {
+        // Addressables.LoadAssetAsyncÇawaitÇ≈ë“ÇøéÛÇØÇÈ
+        var texture = await Addressables.LoadAssetAsync<Texture>(target)
+            .WithCancellation(token);
+
+        _image.texture = texture;
+    }
+}
