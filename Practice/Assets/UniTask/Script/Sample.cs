728bc40b3d14d2b4c2986cf233117be763133b39
MizutamariShutarou <shutaro0415@outlook.jp>
2023-06-08 02:01:26 +0900

    UniTaskおもろい

--- Practice/Assets/UniTask/Script/Sample.cs
+++ Practice/Assets/UniTask/Script/Sample.cs
@@ -6,9 +6,15 @@ public class Sample : MonoBehaviour
     private async void Start()
     {
         Debug.Log("Start");
+        await DoAsync();
+        var one = DoAsync1();
+        var two = DoAsync2();
+        var three = DoAsync3();
 
         // 非同期メソッドを呼び出す
-        await DoAsync();
+        
+
+        await UniTask.WhenAll(one, two, three);
 
         Debug.Log("End");
     }
@@ -18,8 +24,24 @@ public class Sample : MonoBehaviour
         Debug.Log("DoAsync start");
 
         // 1秒待機する
-        await UniTask.Delay(1000);
+        await UniTask.DelayFrame(3000);
 
         Debug.Log("DoAsync end");
+    }
+
+    private async UniTask DoAsync1()
+    {
+        Debug.Log("1");
+        await UniTask.CompletedTask;
+    }
+    private async UniTask DoAsync2()
+    {
+        Debug.Log("2");
+        await UniTask.CompletedTask;
+    }
+    private async UniTask DoAsync3()
+    {
+        Debug.Log("3");
+        await UniTask.CompletedTask;
     }
 }
